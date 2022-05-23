ec6b2f16a8dcaddbb3aba51ca205259363314b7f
14Gold10 <104681642+14Gold10@users.noreply.github.com>
2022-05-23 19:11:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-function computerPlay = {
-  ['rock', 'paper', 'scissors'];
-
-function playRound {
-  // your code here!
+function computerPlay(){
+  var items = ['Rock', 'Paper', 'Scissors'];
+  var item = items[Math.floor(Math.random() * items.length)]; document.write(item);
 }
-
-const playerSelection = "rock"
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-            
+computerPlay();
