08607b5108f43aac466c2b5915903adefa8d73aa
14Gold10 <104681642+14Gold10@users.noreply.github.com>
2022-05-24 20:56:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function computerPlay(){
-  var items = ['Rock', 'Paper', 'Scissors'];
+  var items = ['rock', 'paper', 'scissors'];
   var item = items[Math.floor(Math.random() * items.length)]; document.write(item);
 }
-computerPlay();
+
+let playerSelection = prompt("Enter rock, paper, or scissors");
+let computerSelection = computerPlay();
+
+if(playerSelection == computerSelection){
+  alert("Tie!");
+}
+else if(((playerSelection == rock) && (computerSelection == scissors)) || ((playerSelection == paper) && (computerSelection == rock)) || ((playerSelection = scissors) && (computerSelection = paper))){
+  alert("You win!");
+}
+else {
+  alert("You lose!");
+}
