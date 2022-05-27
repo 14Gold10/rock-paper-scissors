fb01d52270ad5d70df3cb9f4f2d03dd0d389aa9e
14Gold10 <104681642+14Gold10@users.noreply.github.com>
2022-05-26 21:05:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ let playerSelection = prompt("Enter rock, paper, or scissors");
 if((playerSelection == "rock") && (computerSelection == "scissors")) {
   alert("You win! Rock beats scissors.");
 }
-else if((playerSelection == "rock") && (computerSelection == "paper")) {
+if((playerSelection == "rock") && (computerSelection == "paper")) {
   alert("You lose! Rock is beaten by paper.");
 }
-else if((playerSelection == "paper") && (computerSelection == "rock")) {
+if((playerSelection == "paper") && (computerSelection == "rock")) {
   alert("You win! Paper beats rock.");
 }
-else if((playerSelection == "paper") && (computerSelection == "scissors")) {
+if((playerSelection == "paper") && (computerSelection == "scissors")) {
   alert("You lose! Paper is beaten by scissors.");
 }
-else if((playerSelection == "scissors") && (computerSelection == "paper")) {
+if((playerSelection == "scissors") && (computerSelection == "paper")) {
   alert("You win! Scissors beats paper!");
 }
-else if((playerSelection == "scissors") && (computerSelection == "rock")) {
+if((playerSelection == "scissors") && (computerSelection == "rock")) {
   alert("You lose! Scissors is beaten by rock.");
 }
-else {
+if(playerSelection == computerSelection) {
   alert("Tie!");
 }
