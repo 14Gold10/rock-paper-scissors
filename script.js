6c7cb46aa5b4a6a57b1b8da05e6269379154b17f
14Gold10 <104681642+14Gold10@users.noreply.github.com>
2022-05-30 22:33:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,22 +7,22 @@ let computerSelection = computerPlay();
 let playerSelection = prompt("Enter rock, paper, or scissors");
 return prompt(playerSelection);
 
-if ((playerSelection == "rock") && (computerSelection == "scissors")) {
+if ((playerSelection === "rock") && (computerSelection === "scissors")) {
   document.write("You win! Rock beats scissors.");
 }
-else if ((playerSelection == "rock") && (computerSelection == "paper")) {
+else if ((playerSelection === "rock") && (computerSelection === "paper")) {
   document.write("You lose! Rock is beaten by paper.");
 }
-else if ((playerSelection == "paper") && (computerSelection == "rock")) {
+else if ((playerSelection === "paper") && (computerSelection === "rock")) {
   document.write("You win! Paper beats rock.");
 }
-else if ((playerSelection == "paper") && (computerSelection == "scissors")) {
+else if ((playerSelection === "paper") && (computerSelection === "scissors")) {
  document.write("You lose! Paper is beaten by scissors.");
 }
-else if((playerSelection == "scissors") && (computerSelection == "paper")) {
+else if((playerSelection === "scissors") && (computerSelection === "paper")) {
   document.write("You win! Scissors beats paper!");
 }
-else if((playerSelection == "scissors") && (computerSelection == "rock")) {
+else if((playerSelection === "scissors") && (computerSelection === "rock")) {
   document.write("You lose! Scissors is beaten by rock.");
 }
 else {
